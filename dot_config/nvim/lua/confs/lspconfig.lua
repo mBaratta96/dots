e1baef5e960b33fe105bacde118fd19eb52a8abc
mBaratta96 <marcobaratta96@gmail.com>
2023-10-13 19:02:29 +0200

    fix

--- dot_config/nvim/lua/confs/lspconfig.lua
+++ dot_config/nvim/lua/confs/lspconfig.lua
@@ -75,4 +75,50 @@ M.volar = function()
 		filetypes = { "vue" },
 	})
 end
+M.sign_columns = function()
+	---custom namespace
+	local ns = vim.api.nvim_create_namespace("severe-diagnostics")
+
+	---reference to the original handler
+	local orig_signs_handler = vim.diagnostic.handlers.signs
+	filter_diagnostics = function(diagnostics)
+		if not diagnostics then
+			return {}
+		end
+
+		-- find the "worst" diagnostic per line
+		local most_severe = {}
+		for _, cur in pairs(diagnostics) do
+			local max = most_severe[cur.lnum]
+
+			-- higher severity has lower value (`:h diagnostic-severity`)
+			if not max or cur.severity < max.severity then
+				most_severe[cur.lnum] = cur
+			end
+		end
+
+		---Overriden diagnostics signs helper to only show the single most relevant sign
+		---@see `:h diagnostic-handlers`
+		vim.diagnostic.handlers.signs = {
+			show = function(_, bufnr, _, opts)
+				-- get all diagnostics from the whole buffer rather
+				-- than just the diagnostics passed to the handler
+				local diagnostics = vim.diagnostic.get(bufnr)
+
+				local filtered_diagnostics = filter_diagnostics(diagnostics)
+
+				-- pass the filtered diagnostics (with the
+				-- custom namespace) to the original handler
+				orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+			end,
+
+			hide = function(_, bufnr)
+				orig_signs_handler.hide(ns, bufnr)
+			end,
+		}
+
+		-- return list of diagnostics
+		return vim.tbl_values(most_severe)
+	end
+end
 return M
--- dot_config/nvim/lua/plugins/lsp-zero.lua
+++ dot_config/nvim/lua/plugins/lsp-zero.lua
@@ -1,50 +1,3 @@
-function show_only_one_sign_in_sign_column()
-	---custom namespace
-	local ns = vim.api.nvim_create_namespace("severe-diagnostics")
-
-	---reference to the original handler
-	local orig_signs_handler = vim.diagnostic.handlers.signs
-
-	---Overriden diagnostics signs helper to only show the single most relevant sign
-	---@see `:h diagnostic-handlers`
-	vim.diagnostic.handlers.signs = {
-		show = function(_, bufnr, _, opts)
-			-- get all diagnostics from the whole buffer rather
-			-- than just the diagnostics passed to the handler
-			local diagnostics = vim.diagnostic.get(bufnr)
-
-			local filtered_diagnostics = filter_diagnostics(diagnostics)
-
-			-- pass the filtered diagnostics (with the
-			-- custom namespace) to the original handler
-			orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
-		end,
-
-		hide = function(_, bufnr)
-			orig_signs_handler.hide(ns, bufnr)
-		end,
-	}
-
-	filter_diagnostics = function(diagnostics)
-		if not diagnostics then
-			return {}
-		end
-
-		-- find the "worst" diagnostic per line
-		local most_severe = {}
-		for _, cur in pairs(diagnostics) do
-			local max = most_severe[cur.lnum]
-
-			-- higher severity has lower value (`:h diagnostic-severity`)
-			if not max or cur.severity < max.severity then
-				most_severe[cur.lnum] = cur
-			end
-		end
-
-		-- return list of diagnostics
-		return vim.tbl_values(most_severe)
-	end
-end
 return {
 	{
 		"VonHeikemen/lsp-zero.nvim",
@@ -76,7 +29,9 @@ return {
 			local null_ls = require("confs.null-fs")
 
 			null_ls.setLsp(lsp_zero)
-			show_only_one_sign_in_sign_column()
+
+			local lsp_confs = require("confs.lspconfig")
+			lsp_confs.sign_columns()
 			lsp_zero.on_attach(function(client, bufnr)
 				-- see :help lsp-zero-keybindings
 				-- to learn the available actions
@@ -84,7 +39,6 @@ return {
 				require("navigator.lspclient.mapping").setup({ bufnr = bufnr, client = client })
 			end)
 			require("mason").setup({})
-			local lsp_confs = require("confs.lspconfig")
 			require("mason-lspconfig").setup({
 				ensure_installed = { "tsserver", "rust_analyzer" },
 				handlers = {
