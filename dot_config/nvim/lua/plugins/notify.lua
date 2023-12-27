a4baf049253e56ae2af48b78db529eaef60ba269
mBaratta96 <marcobaratta96@gmail.com>
2023-12-27 15:11:24 +0100

    update

--- dot_config/nvim/lua/plugins/notify.lua
+++ dot_config/nvim/lua/plugins/notify.lua
@@ -10,16 +10,14 @@ end
 return {
 	"rcarriga/nvim-notify",
 	config = function()
+		vim.notify = require("notify")
+		require("notify").setup({ render = "wrapped-compact" })
 		local today = os.date("%Y-%m-%d")
 		local path = "/home/marco/Documenti/projects/personal/daily/" .. today .. ".md"
 		if not file_exists(path) then
-			require("notify")(
-				"No daily journal entry has been found.\nConsider adding something.",
-				vim.log.levels.WARN,
-				{
-					title = "Journal",
-				}
-			)
+			vim.notify("No daily journal entry has been found.\nConsider adding something.", vim.log.levels.WARN, {
+				title = "Journal",
+			})
 		end
 	end,
 	lazy = false,
